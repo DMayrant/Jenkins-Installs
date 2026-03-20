be4056bb48e59fcad799cc295f661b6d4b1ae904
Devonne Mayrant <DMayrant@icloud.com>
2026-03-20 14:36:40 +0700

    Update installs.sh

--- installs.sh
+++ installs.sh
@@ -18,6 +18,12 @@ source venv/bin/activate
 pip install checkov
 checkov --version
 
+apt update
+apt install -y wget unzip
+wget https://releases.hashicorp.com/terraform/1.6.6/terraform_1.6.6_linux_amd64.zip
+unzip terraform_1.6.6_linux_amd64.zip
+mv terraform /usr/local/bin/
+
 chown root:docker /var/run/docker.sock &&
 chmod 660 /var/run/docker.sock &&
 ls -l /var/run/docker.sock &&
